9e72806b8ea28f47a2a9db9920228734aaf5f005
Seow Yong Tao <seowyongtao22@gmail.com>
2021-10-19 15:57:27 +0800

    done home route display

--- server.py
+++ server.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def testing():
-    return "Hello Testing"
+def home_route():
+    return "<h1>Guess a number between 0 and 9</h1>" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif' width=200>"
 
 
 if __name__ == "__main__":
